88fc447acd6ae607df6e5d84e85ab22545ba2ab2
Aitan Grossman <aitan.gros@gmail.com>
2019-02-07 23:41:00 -0800

    Fixed errors and replaced swipe list with simpler flat list

--- react-app/App.js
+++ react-app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Welcome from './pages/Welcome'
 import SignUp from './pages/SignUp'
 import DiscoverRecipes from './pages/DiscoverRecipes'
+import PreviewRecipe from './pages/PreviewRecipe'
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { Provider } from 'react-redux'
 import store from './redux/store'
--- react-app/pages/PreviewRecipe.js
+++ react-app/pages/PreviewRecipe.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {BUTTON_BACKGROUND_COLOR, BACKGROUND_COLOR} from '../common/SousChefColors'
 import { StyleSheet, Image, Text, View, ScrollView, FlatList } from 'react-native';
+import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';
 import { beginRecipePreviewFetch } from '../redux/actions/RecipeAction';
 import { connect } from 'react-redux';
 
-class PreviewRecipe extends React.Component {
+export default class PreviewRecipe extends React.Component {
     static navigationOptions = {
         title: "Preview Recipe",
         headerVisible: true,
@@ -20,24 +21,82 @@ class PreviewRecipe extends React.Component {
     }
     constructor(props) {
         super(props);
-        this.state = { recipeID: this.props.navigation.state.recipeID };
-    }
-
-    render() {
-        return (
-
-        )
+        this.state = { recipeID: this.props.navigation.state.id };
     }
 
     componentDidMount() {
         var data = beginRecipePreviewFetch();
-        var data = this.getData();
         this.setState({
-            images: data.images,
+            image: data.images ? data.images.trim() : "",
             ingredients: data.ingredients,
             servings: data.servings,
             time: data.time,
             title: data.title
         });
     }
+
+    render() {
+        return (
+            <View style={[styles.container]}>
+                // Page header
+                <Text style={[styles.sectionHeader]}>{this.state.title}</Text>
+
+                // image
+                <Image source={this.state.image}
+                    style={[styles.image]}/>
+
+                // List of ingredients
+                // TODO: https://www.npmjs.com/package/react-native-swipe-list-view
+                <FlatList
+                    style={[styles.section]}
+                    data={this.state.ingredients}
+                    renderItem={({item}) => {
+                        return (
+                            <View style={[styles.listItem]}>
+                                <View style={[styles.ingredientQuantity]}>
+                                    <Text>{item.quantity} {item.unit}</Text>
+                                </View>
+                                <View style={[styles.ingredientName]}>
+                                    <Text>{item.ingredient}</Text>
+                                </View>
+                            </View>
+                        );
+                    }}
+                />
+
+            </View>
+        );
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: BACKGROUND_COLOR,
+        paddingBottom: 25
+    },
+    section: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    sectionHeader: {
+        fontFamily: "Avenir Next",
+        fontSize: 25,
+        margin: 10
+    },
+    sectionContainer: {
+        flex: 1,
+    },
+    listItem: {
+        fontFamily: "Avenir Next",
+        fontSize: 15,
+        margin: 3
+    },
+    ingredientQuantity: {
+        fontWeight: bold,
+        fontSize: 10
+    },
+    ingredientName: {
+    }
+});
