9f617c2a0c37d36c6e9350c850dd8ac76d01a343
Aitan Grossman <aitan.gros@gmail.com>
2019-02-18 20:33:09 -0800

    First stab at filtering ingredients by have and don't have

--- react-app/pages/PreviewRecipe.js
+++ react-app/pages/PreviewRecipe.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {BUTTON_BACKGROUND_COLOR, BACKGROUND_COLOR} from '../common/SousChefColors'
-import { StyleSheet, Image, Text, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Image, Text, View, ScrollView, FlatList, Dimensions } from 'react-native';
 import firebase from 'react-native-firebase';
 import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';
 import { beginRecipePreviewFetch } from '../redux/actions/RecipeAction';
 import { connect } from 'react-redux';
 
 const recipesRef = firebase.firestore().collection('recipes');
+const pantryRef = firebase.firestore().collection('pantrylists');
+const mappingsRef = firebase.firestore().collection('standardmappings');
+
+const win = Dimensions.get('window');
 
 export default class PreviewRecipe extends React.Component {
     static navigationOptions = {
@@ -26,7 +30,10 @@ export default class PreviewRecipe extends React.Component {
         super(props);
         this.state = {
             recipeID: this.props.navigation.state.id,
-            recipe: null
+            recipe: null,
+            image: "",
+            imageWidth: 0,
+            imageHeight: 0
         };
     }
 
@@ -34,39 +41,145 @@ export default class PreviewRecipe extends React.Component {
         var recipeID = "0063ec25-5e33-4a59-9a52-ecd090c3fcad";
 
         recipesRef.doc(recipeID).get().then((doc) => {
-            this.setState({recipe: doc.data()});
-            console.log("----------INGREDIENTS-------------");
-            console.log(doc.data());
+            var data = doc.data();
+            var ingredientsArray = Object.values(data.ingredients);
+            data.ingredients = ingredientsArray;
+            this.setState({
+                recipe: data,
+                image: data.images ? data.images : "https://images.media-allrecipes.com/userphotos/560x315/2345230.jpg"
+            });
         })
         .catch(function(error) {
             console.warn("Error getting documents: ", error);
         });
+
+
     }
+
+    isInPantry(ingrData, pantryIngrData) {
+        // Search for standard mappings of ingredient
+        mappingsRef.doc(ingrData.ingredient).get().then((doc) => {
+            var data = doc.data();
+            var ingrDescription = ingrData.ingredient +
+            var convRatio = data.standardTo[]
+        });
+        var quantity = ingrData.quantity;
+        var unit = ingrData.unit;
+    }
+
+    // Sort required ingredients into enough and not enough
+    getSurpluses() {
+        return this.state.recipe.ingredients.map((recipeIngrData) => {
+            // Search for item in pantry
+            var surplus = null;
+            pantryRef.doc(this.props.userID).collection("ingredients")
+                .doc(recipeIngrData.ingredient).get().then((pantryIngrDoc) => {
+                    if (pantryIngrDoc.exists) {
+                        var pantryIngrData = pantryIngrDoc.data();
+                        // Figure out if we have enough
+                        surplus = pantryIngrData.amount -
+                            recipeIngrData.amount;
+                    }
+                }).catch(function(error) {
+                });
+
+            if (surplus == null) {
+                // We don't have this ingredient at all
+                surplus = -recipeIngrData.amount;
+            }
+
+            return surplus;
+        });
+    }
+
+    createIngredientList(haveIngredients, dontHaveIngredients) {
+        var elements = [];
+
+        elements.push(
+            <Text style={[styles.ingredientsLabel]}>
+                You have:
+            </Text>
+        );
+
+        haveIngredients.forEach((elem) => {
+            [item, surplus] = elem;
+            elements.push(
+                <Text
+                    style={[styles.ingredientName]}
+                    data={{"surplus": surplus}}
+                    key={item.ingredient}>
+                    {item.quantity} {item.unit} {item.ingredient}
+                </Text>
+            );
+        });
+
+        elements.push(
+            <Text style={[styles.ingredientsLabel]}>
+                You don't have:
+            </Text>
+        );
+
+        dontHaveIngredients.forEach((elem) => {
+            [item, surplus] = elem;
+            elements.push(
+                <Text
+                    style={[styles.ingredientName]}
+                    data={{"surplus": surplus}}
+                    key={item.ingredient}>
+                    {item.quantity} {item.unit} {item.ingredient}
+                </Text>
+            );
+        });
+
+        return elements;
+    }
+
+    listIngredients() {
+        if (this.state.recipe.ingredients == null){
+            console.warn("null");
+        }
+
+        // surpluses[i] == (we have enough of this.state.recipe.ingredients[i])
+        var surpluses = getSurpluses();
+
+        // Sort ingredients into have and don't have
+        var haveIngredients = [];
+        var dontHaveIngredients = [];
+        surpluses.forEach((surplus, i) => {
+            if (surplus >= 0) {
+                // We have enough of ingredient at index i
+                haveIngredients.push([
+                    this.state.recipe.ingredients[i],
+                    surplus
+                ]);
+            }
+            else {
+                // We don't have enough of ingredient at index i
+                donthHaveIngredients.push([
+                    this.state.recipe.ingredients[i],
+                    surplus
+                ]);
+            }
+        });
+
+        // Construct XML to return
+        return createIngredientList();
+    }
+
     // TODO: https://www.npmjs.com/package/react-native-swipe-list-view
     render() {
-        if (this.state.recipe) {
+        if (this.state.recipe && this.state.recipe.ingredients) {
             return (
                 <View style={[styles.container]}>
-                    <Text style={[styles.sectionHeader]}>{this.state.title ? this.state.title : "unknown"}</Text>
-                    <Image source={this.state.image}
-                        style={[styles.image]}/>
-                    <FlatList
-                        style={[styles.section]}
-                        data={this.state.recipe.ingredients}
-                        renderItem={({item}) => {
-                            return (
-                                <View style={[styles.listItem]}>
-                                    <View style={[styles.ingredientQuantity]}>
-                                        <Text>{item.quantity + " " + item.unit}</Text>
-                                    </View>
-                                    <View style={[styles.ingredientName]}>
-                                        <Text>{item.ingredient ? item.ingredient : "unknown"}</Text>
-                                    </View>
-                                </View>
-                            );
-                        }}
-                    />
-
+                    <Text style={[styles.sectionHeader]}>
+                        {this.state.recipe.title ? this.state.recipe.title : "unknown"}
+                    </Text>
+                    <View style={[styles.imageContainer]}>
+                        <Image source={{uri: this.state.image}}
+                            style={[styles.image]}/>
+                    </View>
+                    {this.listIngredients()}
+                    
                 </View>
             );
         }
@@ -96,6 +209,8 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     listItem: {
+        borderColor: "red",
+        borderWidth: 1,
         fontFamily: "Avenir Next",
         fontSize: 15,
         margin: 3
@@ -105,5 +220,16 @@ const styles = StyleSheet.create({
         fontSize: 10
     },
     ingredientName: {
+    },
+    ingredientsLabel: {
+
+    },
+    imageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    image: {
+        width: 560,
+        height: 315
     }
 });
